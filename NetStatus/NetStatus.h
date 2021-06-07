4e5617b9ff768fbe86fabb1a21e86ec3321fd395
Jyoti purohit <jyoti.purohit@perennialsys.com>
2021-06-07 14:00:42 +0530

    Added framework source and podspec

---
+++ NetStatus/NetStatus.h
@@ -0,0 +1,18 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by Apple on 07/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NetStatus
 A small framework for monitoring network changes in swift.
+
+created by Jyoti Purohit
